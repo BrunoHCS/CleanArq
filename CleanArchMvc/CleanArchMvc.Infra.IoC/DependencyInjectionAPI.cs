83738c149b1ef1ed8d2ffce376ae6e95f07603d6
Bruno Henrique <bruno_hcs2009@hotmail.com>
2023-02-22 22:46:28 -0300

    Implementando a camada API

---
+++ CleanArchMvc/CleanArchMvc.Infra.IoC/DependencyInjectionAPI.cs
@@ -0,0 +1,48 @@
+﻿using CleanArchMvc.Application.Intefaces;
+using CleanArchMvc.Application.Mappings;
+using CleanArchMvc.Application.Services;
+using CleanArchMvc.Domain.Account;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Identity;
+using CleanArchMvc.Infra.Data.Repositories;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CleanArchMvc.Infra.IoC
+{
+    public static class DependencyInjectionAPI
+    {
+        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(
+                configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext)
+                .Assembly.FullName)));
+
+            service.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            service.AddScoped<IProductRepository, ProductRepository>();
+            service.AddScoped<ICategoryRepository, CategoryRepository>();
+
+            service.AddScoped<IProductService, ProductService>();
+            service.AddScoped<ICategoryService, CategoryService>();
+
+            service.AddScoped<IAuthenticate, AuthenticateService>();
+
+            service.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            var myhandlers = AppDomain.CurrentDomain.Load("CleanArchMvc.Application");
+            service.AddMediatR(myhandlers);
+
+            return service;
+        }
+    }
+}
